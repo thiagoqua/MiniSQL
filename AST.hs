9bd4681b9c6f93876ba1da1874dfb769eadc1f38
tiki <thiagoqua16@gmail.com>
2023-06-28 20:44:37 -0300

    Correcciones #1.

--- AST.hs
+++ AST.hs
@@ -2,27 +2,68 @@ module AST where
 
 -- CONSULTAS SQL
 {-
-    SELECT * FROM Table1
-    SELECT "column_name" FROM "table_name";
-    SELECT "column_name1", "column_name2" FROM "table_name";
-    SELECT * FROM “table_name” WHERE “column_name” = “nombre”
-    SELECT * FROM “table_name” WHERE “column_name” = “nombre” AND “column_name1” = “nombre”
-    SELECT * FROM “table_name” WHERE “column_name” = “nombre” OR “column_name1” = “nombre”
-    SELECT * FROM “table_name” WHERE NOT “column_name” = “nombre”
-    SELECT pv.ProductID, v.BusinessEntityID, v.Name FROM Purchasing.ProductVendor AS pv, Purchasing.Vendor AS v WHERE pv.BusinessEntityID=v.BusinessEntityID
-    SELECT TOP “number” * FROM “table_name”
-    SELECT * FROM “table_name” ORDER BY “column_name” BY ASC
-    SELECT * FROM “table_name” ORDER BY “column_name” BY DESC
-    SELECT * FROM “table_name” GROUP BY “nombre”
-    CREATE DATABASE “NombreBDD”;
-    CREATE TABLE “table_name” (nombre_columna tipodato(largo));
-    INSERT INTO “table_name” VALUES (“date1, date2, date3”);
-    DELETE FROM table_name;
-    DELETE FROM table_name WHERE condition;
+    SELECT * 
+        FROM Table1
+    SELECT "column_name" 
+        FROM "table_name";
+    SELECT "column_name1", "column_name2" 
+        FROM "table_name";
+    SELECT * 
+        FROM “table_name” 
+        WHERE “column_name” = “nombre”
+    SELECT * 
+        FROM “table_name” 
+        WHERE “column_name” = “nombre” AND 
+              “column_name1” = “nombre” 
+    SELECT * 
+        FROM “table_name” 
+        WHERE “column_name” = “nombre” OR 
+              “column_name1” = “nombre”
+    SELECT * 
+        FROM “table_name” 
+        WHERE NOT “column_name” = “nombre”
+    SELECT pv.ProductID, v.BusinessEntityID, v.Name 
+        FROM Purchasing.ProductVendor AS pv, 
+             Purchasing.Vendor AS v 
+        WHERE pv.BusinessEntityID = v.BusinessEntityID
+    SELECT TOP “number” * 
+        FROM “table_name”
+    SELECT * 
+        FROM “table_name” 
+        ORDER BY “column_name” BY ASC
+    SELECT * 
+        FROM “table_name” 
+        ORDER BY “column_name” BY DESC
+    SELECT * 
+        FROM “table_name” 
+        GROUP BY “nombre”
+    
+    CREATE DATABASE "db_name”;
+    CREATE TABLE table_name ("column_name" dataType(largo));
+    
+    INSERT INTO “table_name” 
+        VALUES (“data1", "data2", "data3”);
+    
+    DELETE FROM "table_name";
+    DELETE FROM "table_name"
+        WHERE condition;
 -}
 
-data IntExp = Const Integer
- deriving (Show,Eq)
+-- Nuestros tipos
+type HeterList = [Types]
+type Name = String
+type TableName = String
+type DatabaseName = String
+type ColumnName = String
+type DataType = String
+type DataLong = Int
+type Alias = String
+type Op = Char   -- >,<,=,>=,<=,!=
+
+-- Estructuras de datos
+-- Datos soportados
+data Types = S String | N Integer | B Bool | F Float
+
 
 -- Expresiones Booleanas
 data BoolExp = BTrue
@@ -33,63 +74,55 @@ data BoolExp = BTrue
              | Not BoolExp
  deriving (Show,Eq)
 
+data IntExp = Const Integer
+ deriving (Show,Eq)
+
 -- COMANDOS
-data Command = Select SelQueries
-             | CreateDatabase String
-             | CreateTable String [CreateTableQuery]
-             | Insert String [String]
-             | Delete Del
+data Command = Select SelectBody
+             | CreateDatabase DatabaseName
+             | CreateTable TableName [ColumnCreation]
+             | Insert ColumnName [HeterList]
+             | Delete TableName Cond
  deriving Show
+
+--CONDICION
+------------------------- TODO ----------------
+data Cond = Skip
+          | Exp Op Name Types
 
 --CREATE TABLE
-data CreateTableQuery = CreateTable_ String String Int
-                      | CreateTable1_ String String
+data ColumnCreation = ColumnString ColumnName DataType DataLong
+                    | ColumnOthers ColumnName DataType
  deriving Show
 
---SELECT
-data SelQueries = SelQ_ Asterisk
-                | SelQ1_ ColumnMaster
-                | SelQ2_ Top From
+-- SELECT
+-- OPCIONES
+data SelectOpc = Column ColumnOptions
+               | Top Integer ColumnOptions
+
+data ColumnOptions = Asterisk 
+                   | Columns [ColumnName]
+
+-- CUERPO
+data SelectBody = Body SelectOpc TableName Cond
  deriving Show
 
-data Asterisk = Ast_ Char From
-              | Ast1_ Char From Where
-              | Ast2_ Char From GroupBy
-              | Ast3_ Char From OrderBy
- deriving Show
-
+------------------------- TODO ----------------
 data OrderBy = OrderBy_ String By
+ deriving Show
+
+data GroupBy = GroupBy_ String
  deriving Show
 
 data By = ASC
         | DESC
  deriving Show
 
-data GroupBy = GroupBy_ String
+
+data As = As_ ColumnName Alias
  deriving Show
 
-data Top = Top_ IntExp
- deriving Show
-
-data ColumnMaster = CM_ Column From Where
-                  | CM1_ Column From
- deriving Show
-
-data Column = Col_ [String]
- deriving Show
-
-data From = From_ String
-          | From1_ [String]
-          | From2_ [As]
- deriving Show
-
-data As = As_ String String
- deriving Show
-
-data Where = Where_ BoolExp
- deriving (Show,Eq)
-
---DELETE
-data Del = Del_ From Where
-         | Del1_ From
- deriving Show
+-- data From = From_ String
+--           | From1_ [String]
+--           | From2_ [As]
+--  deriving Show
